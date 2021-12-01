b45846c675e900a0f4f39e8e3b0a7881a323999f
BJP <50512941+BEOMKING@users.noreply.github.com>
2021-12-01 20:55:25 +0900

    Delete frontend directory

--- frontend/src/utils/phoneNumberValidation.js
+++
@@ -1,8 +0,0 @@
-function validatePhone(phone) {
-	const re =
-		/\d/;
-	return re.test(String(phone).toLowerCase());
-}
-
-export { validatePhone };
- // 한국 전화번호 형식
--- frontend/src/utils/time.js
+++
@@ -1,6 +0,0 @@
-function timestamp(){
-    let today = new Date(); today.setHours(today.getHours() + 9); 
-    return today.toISOString().replace('T', ' ').substring(0, 19); }
-export{
-    timestamp
-}
